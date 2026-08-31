2111e1ae3c89f505f71738c3ebf052a16f54910d
6f9 <151858999+rsmdevlin@users.noreply.github.com>
2026-08-31 17:42:22 +0200

    Update Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:3000",
-        changeOrigin: true
+        changeOrigin: true,
+        credentials: true
       }
     }
   },
